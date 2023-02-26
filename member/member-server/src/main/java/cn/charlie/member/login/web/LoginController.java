2e4a34df1171cd54990a4722c600ca811b00465b
charlie <327371999@qq.com>
2023-02-26 10:51:10 +0800

    CHCOBO-7 部署控制器

---
+++ member/member-server/src/main/java/cn/charlie/member/login/web/LoginController.java
@@ -0,0 +1,19 @@
+package cn.charlie.member.login.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author charlie
+ * @date 2/26/2023 10:39 AM
+ **/
+@RestController
+@RequestMapping("/session")
+public class LoginController {
+
+    @GetMapping("")
+    public String getSession() {
+        return "hello world";
+    }
+}
