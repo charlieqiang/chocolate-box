ea58821673969c863e4aac73ecc5c19bc7628a0c
charlie <327371999@qq.com>
2023-03-09 09:19:08 +0800

    CHCOBO-31 集成sentinel CHCOBO-33 Readme文件补充

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # chocolate-box
 
 #### 介绍
-于2023-02-24开始的一份开发习作, 主要用于沉淀自2021年毕业以来的Java后端技术知识.
+于2023-02-24开始的一份开发习作, 主要用于沉淀自2021年毕业以来的Java后端技术知识. 采用主流微服务架构, 集成常用分布式组件, 初步计划搭建后端基础功能, 后期再编写前端. 
 
 #### 软件架构
-软件架构说明, 待补充
+舍去主流parent-api-server架构, 直接采用parent-module, 
 
 #### 安装教程
-待补充
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1. jdk
+2. maven
+3. nacos
+4. mysql
+5. redis
+6. seata
+7. sentinel
+8. rocketMq
+9. gateway
+10. es
 
 #### 使用说明
-待补充
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1.  配置文件涉及公网IP, 没有提交. 需要可以邮件联系: 327371999@qq.com
--- chocolate-order/src/main/java/cn/charlie/order/base/reference/InventoryReference.java
+++ chocolate-order/src/main/java/cn/charlie/order/base/reference/InventoryReference.java
@@ -1,6 +1,7 @@
 package cn.charlie.order.base.reference;
 
 import cn.charlie.common.dubbo.api.storage.invtory.InventoryClient;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,12 @@ public class InventoryReference {
     @Reference
     private InventoryClient inventoryClient;
 
+    @SentinelResource(value = "OperateInventoryByItemId", fallback = "operateInventoryByItemIdFallBack")
     public Boolean operateInventoryByItemId(Long itemId, Integer operateQty) {
         return inventoryClient.operateInventoryByItemId(itemId, operateQty);
+    }
+
+    public Boolean operateInventoryByItemIdFallBack(Long itemId, Integer operateQty, Throwable e) throws Exception {
+        throw new Exception("限流");
     }
 }
